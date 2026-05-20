476f9050d03332095e3ed131bbc022d8fe0f175c
AnthonyOff <130919565+Anthonyoff380@users.noreply.github.com>
2026-05-20 16:26:36 +0200

    Delete js directory

--- js/personnels.js
+++
@@ -1,4 +0,0 @@
-/* ============================================================
-   SYSTEL POMPIERS - MODULE PERSONNELS (v17)
-   Les personnels sont gérés via USERS dans app.js / admin
-   ============================================================ */
