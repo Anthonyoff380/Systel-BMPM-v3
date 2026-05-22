a497a4432138e39c3155f1bb2d8f08ddd361c95d
AnthonyOff <130919565+Anthonyoff380@users.noreply.github.com>
2026-05-22 18:13:01 +0200

    Delete js directory

--- js/annuaire.js
+++
@@ -1,4 +0,0 @@
-/* ============================================================
-   SYSTEL POMPIERS - MODULE ANNUAIRE (v17)
-   ============================================================ */
-// renderAnnuaire est défini dans app.js - ce fichier garde les éventuelles extensions
